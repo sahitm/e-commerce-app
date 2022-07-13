671c04998eccc03a19cc07b927e77e2e8deb30a4
sahitm <sahit7varma@gmail.com>
2022-07-13 15:38:34 +0530

    created a header & added links to route between photos page and cart page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import './App.css';
+import './App.css'
+import React from "react"
+import Header from './components/Header'
+import Cart from './components/Cart'
+import Photos from './components/Photos'
+import { Routes, Route } from 'react-router-dom'
+
 
 function App() {
   return (
     <div>
-      hello world
+      <Header />
+      {/* <Switch>
+          <Route >
+            <Photos exact path="/"/>
+          </Route>
+
+          <Route exact path="/cart">
+            <Cart />
+          </Route>
+
+      </Switch> */}
+      <Routes>
+        <Route path="/" element={<Photos />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
     </div>
-  );
+ 
+  )
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+function Header() {
+    return (
+        <header>
+            
+            <h2><Link to="/">Photozon</Link></h2>
+            <Link to="/cart"><i className="ri-shopping-cart-line ri-fw ri-2x"></i></Link>
+           
+        </header>
+    )
+}
+
+export default Header;
